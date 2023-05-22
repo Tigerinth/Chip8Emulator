0bb8961d96c34008e91c7a685a6968002ec12cd7
Emir <emirkpln5402@gmail.com>
2023-05-22 23:28:25 +0300

    class commit

---
+++ Chip8Emulator/Chip8.cpp
@@ -0,0 +1,12 @@
+﻿// Chip8.cpp : Defines the entry point for the application.
+//
+
+#include "Chip8.h"
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
---
+++ Chip8Emulator/Chip8.h
@@ -0,0 +1,32 @@
+﻿// Chip8.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
+class Chip8 {
+
+public:
+	uint8_t registers[REGISTER_COUNT]{};
+	uint8_t memory[MEMORY_SIZE]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint16_t stack[STACK_LEVELS]{};
+	uint8_t sp{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint8_t keypad[KEY_COUNT]{};
+	uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{};
+	uint16_t opcode;
+
+};
+
+
+// TODO: Reference additional headers your program requires here.
